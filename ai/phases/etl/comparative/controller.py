d487fef984623f15eb7f6d04bee5d92d927ee281
Miguel Amato <miamato@ucm.es>
2025-04-16 20:08:08 +0200

    refactor(ai): delete some docs.

--- ai/phases/etl/comparative/controller.py
+++ ai/phases/etl/comparative/controller.py
@@ -5,44 +5,9 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../..")))
 
 
-# Categoric
-from ai.phases.etl.comparative.categorical.agent import ComparativeCategoricalAgent
-from ai.phases.etl.comparative.numerical.agent import ComparativeNumericalAgent
-
 from ai.phases.etl.comparative.categorical.chi_square import ChiSquareComparativeAgent
 from ai.phases.etl.comparative.categorical.fisher import FisherExactComparativeAgent
 
-def execute() -> dict:
-    """Main call to the API. EXECUTE THIS FUNCTION!"""
-    categorical_response = run_categorical_analysis()
-    print(categorical_response)
-    numerical_response = run_numerical_analysis({ 
-        "name": categorical_response["results"]["group_variable"], 
-        "valid_keys": categorical_response["results"]["valid_keys"],
-        }
-    )
-    return {
-        "categorical_analysis": categorical_response,
-        "numerical_analysis": numerical_response
-    }
-
-
-def run_categorical_analysis() -> dict:
-	"""Call the LLM Agent for the numeric analysis.
-
-	Returns:
-		dict: returns the report and result of the numeric analysis.
-	"""
-	agent = ComparativeCategoricalAgent()
-	response = agent.execute()
-	return response
-
-def run_numerical_analysis(group_variable: dict) -> dict:
-    if not group_variable :
-        return { "error" : "You have to execute run_categorical_analysis first"}
-    agent = ComparativeNumericalAgent(group_variable=group_variable)
-    response = agent.execute()
-    return response
 
 def run_chi_square_analysis() -> dict:
     """Calls the execute function of the LLM agent to do the chi square test
