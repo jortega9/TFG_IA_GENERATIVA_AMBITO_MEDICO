a30e0086b85365b026829bd00711db265b88b2ac
Miguel Amato <miamato@ucm.es>
2025-04-20 17:25:36 +0200

    feat(survival): cox univariant analysis done

--- ai/phases/etl/survival/controller.py
+++ ai/phases/etl/survival/controller.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 
 
 from ai.phases.etl.survival.kaplan_meier.agent import KaplanMeierAgent 
+from ai.phases.etl.survival.cox.agent import COXAgent 
 
 def run_kaplan_meier_analysis() -> dict:
     """Run Kaplan-Meier survival analysis."""
@@ -13,12 +14,17 @@ def run_kaplan_meier_analysis() -> dict:
     response = agent.execute()
     return response
 
+def run_cox_analysis() -> dict:
+    """Run regetion COX analysis"""
+    agent = COXAgent()
+    response = agent.execute()
+    return response
 
 #########################################################################################
 #                                   DELETE ME                                           #
 #########################################################################################   
 def main() :
-	print(run_kaplan_meier_analysis())
+	print(run_cox_analysis())
     
 if __name__ == "__main__":
     main()
